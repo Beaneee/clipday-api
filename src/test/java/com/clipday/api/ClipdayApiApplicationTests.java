44f347858e7d412882127d7c0d6b28991aa94eea
종빈 <whddkrak@gmail.com>
2026-07-23 22:11:19 +0900

    chor: project init

---
+++ src/test/java/com/clipday/api/ClipdayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clipday.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipdayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
